3c7fa407a27c974ee8619b3ab037a91a25050761
El chavacaba <salvadorcoscab@gmail.com>
2022-11-30 12:22:26 -0600

    [test] Prueba para conectar netbeans con github

---
+++ src/main/java/proyectoautomata/Practica2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoautomata;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author salvatorecoscab
+ */
+public class Practica2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    static Scanner ent = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String palabra = ent.nextLine();
+        System.out.println("hola");
+
+    }
+    
+}
