b6a5cc5006275bb40ef3774719e0124565966966
El chavacaba <salvadorcoscab@gmail.com>
2022-12-03 14:33:25 -0600

    [features] New menus

--- src/main/java/proyectoautomata/Alfabeto.java
+++ src/main/java/proyectoautomata/Alfabeto.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyectoautomata;
-import java.util.ArrayList;
+import java.util.ArrayLisst;
 /**
  *
  * @author salvatorecoscab
